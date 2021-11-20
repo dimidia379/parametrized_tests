1077360eaaa131dd944767dd040807242b4c3482
dimidia379 <j.zvereva050@gmail.com>
2021-11-20 13:59:42 +0300

    completed homework

---
+++ src/test/java/guru/qa/Email.java
@@ -0,0 +1,18 @@
+package guru.qa;
+
+public enum Email {
+    MAILWITHOUTAT ("test.mail.com"),
+    MAILWITHCYRILLIC ("тест@mail.com"),
+    MAILWITHBACKSLASH ("te st@mail.com");
+
+
+    private String title;
+
+    Email(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
